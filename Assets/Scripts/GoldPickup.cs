ea25c48741c72227313eab96fe67611a02d2ffc7
Shyronzo <59961717+Shyronzo@users.noreply.github.com>
2021-11-07 15:34:56 +0100

    Gold having weight, Prefab and speed
    too much slows you down and the prefab works for the physical version
    
    both can be mixed together on the same level extra info on how to do it on the gold.

---
+++ Assets/Scripts/GoldPickup.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldPickup : MonoBehaviour
+{
+    public PickupItem Player;
+    [Header("PickupA, says if we pick it up with hands or invisibly  True to hold, false to vacuum")]
+    public bool PickupA;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").GetComponent<PickupItem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if(PickupA == false)
+            {
+                Player.Gold++;
+                Player.recentGold++;
+                gameObject.SetActive(false);
+            }
+            
+
+        }
+    }
+}
