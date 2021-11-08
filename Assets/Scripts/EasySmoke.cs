9ec689c9847dc8690513b99b1638140290e07084
Henrik <theporkchopsxd@gmail.com>
2021-11-08 10:07:00 +0100

    non instant smoke

--- Assets/Scripts/EasySmoke.cs
+++ Assets/Scripts/EasySmoke.cs
@@ -12,12 +12,25 @@ public class EasySmoke : MonoBehaviour
     // declare the amount of distance the smoke will travel
     public float distance = 1.0f;
 
+    // declare the amount of smoke the player can breathe without dying
+    public float maxBreath = 10.0f;
+
+    // declare a private field to store the current amount of air the player has, and make it visible in the inspector
+    [SerializeField]
+    private float currentBreath = 0.0f;
+
+    // declare a private field to store whether or not the player is currently in the smoke
+    private bool inSmoke = false;
+
     // declare a private field to set the speed of the smoke
     private float speed = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
+        // set the current breath to the max breath
+        currentBreath = maxBreath;
+
         // enable the renderer of the smoke
         GetComponent<Renderer>().enabled = true;
 
@@ -28,6 +41,25 @@ public class EasySmoke : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        // if the player is in the smoke, decrease the current breath by the time
+        if (inSmoke)
+        {
+            currentBreath -= Time.deltaTime;
+            // if the current breath is less than 0, the player is dead
+            if (currentBreath <= 0.0f)
+            {
+                // disable the renderer of the smoke
+                GetComponent<Renderer>().enabled = false;
+                // disable the script
+                enabled = false;
+            }
+        }
+        else
+        {
+            // if the player is not in the smoke, increase the current breath by the time
+            currentBreath += Time.deltaTime;
+        }
+
         // move the smoke downwards at the speed calculated
         transform.Translate(Vector3.down * speed * Time.deltaTime);
     }
@@ -37,9 +69,24 @@ public class EasySmoke : MonoBehaviour
         // if the smoke hits the player
         if (other.gameObject.tag == "Player")
         {
-            // destroy the player then pause the game
-            Destroy(other.gameObject);
-            Time.timeScale = 0;
+            // set the inSmoke field to true
+            inSmoke = true;
+
+            // Log that the player has entered the smoke
+            Debug.Log("Entered the smoke");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        // if the smoke leaves the player
+        if (other.gameObject.tag == "Player")
+        {
+            // set the inSmoke field to false
+            inSmoke = false;
+
+            // log that the player has left the smoke
+            Debug.Log("Left smoke");
         }
     }
 }
