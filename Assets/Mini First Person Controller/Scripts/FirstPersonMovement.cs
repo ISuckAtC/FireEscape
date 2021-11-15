ee9daa0516b3909bf2eac0bb2cd5ea7a0b5862f9
StormTruck <77848378+StormTruck@users.noreply.github.com>
2021-11-15 16:43:20 +0100

    Tweaking FP Con and working on level 1 design

--- Assets/Mini First Person Controller/Scripts/FirstPersonMovement.cs
+++ Assets/Mini First Person Controller/Scripts/FirstPersonMovement.cs
@@ -36,7 +36,10 @@ public class FirstPersonMovement : MonoBehaviour
         }
 
         // Get targetVelocity from input.
-        Vector2 targetVelocity =new Vector2( Input.GetAxis("Horizontal") * targetMovingSpeed, Input.GetAxis("Vertical") * targetMovingSpeed);
+        Vector2 targetVelocity =new Vector2( Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        targetVelocity = targetVelocity.normalized;
+
+        targetVelocity = targetVelocity * targetMovingSpeed;
 
         // Apply movement.
         rigidbody.velocity = transform.rotation * new Vector3(targetVelocity.x, rigidbody.velocity.y, targetVelocity.y);
