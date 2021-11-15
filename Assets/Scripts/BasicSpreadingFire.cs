42963d40a1e783dcd17236051a521b14398197e1
Shyronzo <59961717+Shyronzo@users.noreply.github.com>
2021-11-15 15:58:12 +0100

    adding a basic spreading fire for every spawn
    basic cubes, should be replaced with a prefab or fire objects in their area.

---
+++ Assets/Scripts/BasicSpreadingFire.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class BasicSpreadingFire : MonoBehaviour
+{
+
+    private float FireSpread = 20;
+    private GameObject childFire;
+    // Start is called before the first frame update
+    void Start()
+    {
+        childFire = gameObject.transform.GetChild(0).gameObject;
+        if (childFire != null)
+        {
+            childFire.SetActive(false);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FireSpread -= Time.deltaTime;
+        if(FireSpread <= 0 &&childFire != null)
+        {
+            childFire.SetActive(true);
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            SceneManager.LoadScene("DeathScene");
+        }
+    }
+}
