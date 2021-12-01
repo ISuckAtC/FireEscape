dad81202972b95f2b0583e313503f07824f1749b
Quick-Bolt <nuno.gnu1@gmail.com>
2021-12-01 15:41:12 +0100

    bills :money_with_wings:

--- Assets/Scripts/ExitClearEnd.cs
+++ Assets/Scripts/ExitClearEnd.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class ExitClearEnd : MonoBehaviour
-{ public GameController GC;
+{ 
+    public GameController GC;
     
     public Text ScoreCard;
     public Text GradeText;
     public GameObject goodText, badText, badestText;
     public Text TimerCard;
-    public Image StampA, StampB, StampC, StampF;
+    public Image firstBill, secondBill, thirdBill, winScreen;
     public bool Old;
     // Start is called before the first frame update
     void Start()
     {
         GC = GameObject.Find("GameController").GetComponent<GameController>();
+        firstBill.enabled = false;
+        secondBill.enabled = false;
+        thirdBill.enabled = false;
+        winScreen.enabled = false;
         EndCardLoad();
     }
 
@@ -74,71 +79,71 @@ public class ExitClearEnd : MonoBehaviour
 
         if (GC.Valuables == 0)
         {
-            ScoreCard.text += "You escaped unscathed, congratulations on following good fire safety practices!\n\n";
-            StartCoroutine(GradeA());
+           // ScoreCard.text += "You escaped unscathed, congratulations on following good fire safety practices!\n\n";
+            winScreen.enabled = true;
         }
         else if (GC.Valuables < (GC.maxValuablesForPreviousLevel / 4f))
         {
-            ScoreCard.text += "You managed to escape, however, you have first degree burn marks all over your body and a few second degree ones, the items you picked can only cover for the drive to the hospital (yeah they charge that).";
-            StartCoroutine(GradeF());
+           // ScoreCard.text += "You managed to escape, however, you have first degree burn marks all over your body and a few second degree ones, the items you picked can only cover for the drive to the hospital (yeah they charge that).";
+            firstBill.enabled = true;
         }
         else if (GC.Valuables < (GC.maxValuablesForPreviousLevel * 2f / 4f))
         {
-            ScoreCard.text += "You got out but have several second degree burns on your body, you will mostly likely need surgical intervention, the items you picked up will cover around 30% of the costs.";
-            StartCoroutine(GradeF());
+            //ScoreCard.text += "You got out but have several second degree burns on your body, you will mostly likely need surgical intervention, the items you picked up will cover around 30% of the costs.";
+            secondBill.enabled = true;
         }
         else
         {
-            ScoreCard.text += "You barely managed to escape, you have severe burns all over your body, you'll need immediate surgical intervention and might be incapacitated for the rest of your life, the items you picked up cover 15% of the costs, also you got aids unfortunately..";
-            StartCoroutine(GradeF());
+            //ScoreCard.text += "You barely managed to escape, you have severe burns all over your body, you'll need immediate surgical intervention and might be incapacitated for the rest of your life, the items you picked up cover 15% of the costs, also you got aids unfortunately..";
+            thirdBill.enabled = true;
         }
     }
-    public IEnumerator GradeA()
-    {
-        yield return new WaitForSeconds(2f);
-        Vector3 scale = StampA.transform.localScale;
-        StampA.transform.localScale = Vector3.zero;
-        StampA.enabled = true;
-        while (scale != StampA.transform.localScale)
-        {
-            StampA.transform.localScale = Vector3.MoveTowards(StampA.transform.localScale, scale, Time.deltaTime * 2f);
-            yield return null;
-        }
+    //public IEnumerator GradeA()
+    //{
+    //    yield return new WaitForSeconds(2f);
+    //    Vector3 scale = StampA.transform.localScale;
+    //    StampA.transform.localScale = Vector3.zero;
+    //    StampA.enabled = true;
+    //    while (scale != StampA.transform.localScale)
+    //    {
+    //        StampA.transform.localScale = Vector3.MoveTowards(StampA.transform.localScale, scale, Time.deltaTime * 2f);
+    //        yield return null;
+    //    }
+    //}
+    //public IEnumerator GradeB()
+    //{
+    //    yield return new WaitForSeconds(2f);
+    //    Vector3 scale = StampB.transform.localScale;
+    //    StampB.transform.localScale = Vector3.zero;
+    //    StampB.enabled = true;
+    //    while (scale != StampB.transform.localScale)
+    //    {
+    //        StampB.transform.localScale = Vector3.MoveTowards(StampB.transform.localScale, scale, Time.deltaTime * 2f);
+    //        yield return null;
+    //    }
+    //}
+    //public IEnumerator GradeC()
+    //{
+    //    yield return new WaitForSeconds(2f);
+    //    Vector3 scale = StampC.transform.localScale;
+    //    StampC.transform.localScale = Vector3.zero;
+    //    StampC.enabled = true;
+    //    while (scale != StampC.transform.localScale)
+    //    {
+    //        StampC.transform.localScale = Vector3.MoveTowards(StampC.transform.localScale, scale, Time.deltaTime * 2f);
+    //        yield return null;
+    //    }
+    //}
+    //public IEnumerator GradeF()
+    //{
+    //    yield return new WaitForSeconds(2f);
+    //    Vector3 scale = StampF.transform.localScale;
+    //    StampF.transform.localScale = Vector3.zero;
+    //    StampF.enabled = true;
+    //    while (scale != StampF.transform.localScale)
+    //    {
+    //        StampF.transform.localScale = Vector3.MoveTowards(StampF.transform.localScale, scale, Time.deltaTime * 2f);
+    //        yield return null;
+    //    }
     }
-    public IEnumerator GradeB()
-    {
-        yield return new WaitForSeconds(2f);
-        Vector3 scale = StampB.transform.localScale;
-        StampB.transform.localScale = Vector3.zero;
-        StampB.enabled = true;
-        while (scale != StampB.transform.localScale)
-        {
-            StampB.transform.localScale = Vector3.MoveTowards(StampB.transform.localScale, scale, Time.deltaTime * 2f);
-            yield return null;
-        }
-    }
-    public IEnumerator GradeC()
-    {
-        yield return new WaitForSeconds(2f);
-        Vector3 scale = StampC.transform.localScale;
-        StampC.transform.localScale = Vector3.zero;
-        StampC.enabled = true;
-        while (scale != StampC.transform.localScale)
-        {
-            StampC.transform.localScale = Vector3.MoveTowards(StampC.transform.localScale, scale, Time.deltaTime * 2f);
-            yield return null;
-        }
-    }
-    public IEnumerator GradeF()
-    {
-        yield return new WaitForSeconds(2f);
-        Vector3 scale = StampF.transform.localScale;
-        StampF.transform.localScale = Vector3.zero;
-        StampF.enabled = true;
-        while (scale != StampF.transform.localScale)
-        {
-            StampF.transform.localScale = Vector3.MoveTowards(StampF.transform.localScale, scale, Time.deltaTime * 2f);
-            yield return null;
-        }
-    }
-}
+
